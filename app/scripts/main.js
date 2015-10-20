f6c3416568c6b769234ccadb43dbe51159fe8840
Maxime Beck <maxime.beck@he-arc.ch>
2015-10-20 21:08:57 +0200

    Added sources

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  'use strict';
+  $('a').smoothScroll();
+
+  $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $("#navigation").position();
+        if (scroll > topDist.top) {
+            $('nav').css({"position":"fixed","top":"0"});
+        } else {
+            $('nav').css({"position":"static","top":"auto"});
+        }
+    });
+});
